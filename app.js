29c327cc7e8f69866e4939d0a5eb63be61bceedc
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-06-10 18:13:47 +0200

    Changes of Elko, the index.hbs and small changes on app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
@@ -8,7 +7,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const localhost = process.env.PORT;
 
 mongoose
   .connect('mongodb://localhost/street-curator', {useNewUrlParser: true})
@@ -21,7 +20,6 @@ mongoose
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // Middleware Setup
@@ -31,31 +29,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(__dirname + '/views/partials');
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
 const signup = require('./routes/signup');
 app.use('/', signup);
 
-
 module.exports = app;
 
-app.listen(3000, console.log('Up and running'));
+app.listen(localhost, () => console.log(`App listening on ${localhost}`))
