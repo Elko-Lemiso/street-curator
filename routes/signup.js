1b0681a07ae3105f407a57a2b084c000d8df5095
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-06-10 22:55:14 +0200

    added encryption to the sign up

--- routes/signup.js
+++ routes/signup.js
@@ -7,27 +7,31 @@ router.get('/signup', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) =>{
+    const bcrypt = require('bcrypt');
+    const saltRounds = 10;
+    const myPlaintextPassword = req.body.password;
+    const hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 
-debugger
-
-    User.create({
-        username: `${req.body.username}`,
-        firstName: `${req.body.firstName}`,
-        lastName: `${req.body.lastName}`,
-        email: `${req.body.email}`,
-        password: `${req.body.password}`,
-        userType: `${req.body.userType}`
-    })
+    User
+        .create({
+            username: `${req.body.username}`,
+            firstName: `${req.body.firstName}`,
+            lastName: `${req.body.lastName}`,
+            email: `${req.body.email}`,
+            password: `${hash}`,
+            userType: `${req.body.userType}`
+        })
         .then((user)=>{
             res.redirect('/');
             console.log(user);
         })
         .catch(error =>{
-            console.log();
+            console.log(error);
         })
 })
 
-
-
-
 module.exports = router;
+
+// bcrypt.compare(myPlaintextPassword, hash).then(function(result) {
+//     // result == true
+// });
