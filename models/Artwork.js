64beab53447efce94a2c804c7f128630cfcd7fdb
Elko Lemiso <elkovandongen@gmail.com>
2020-06-10 15:50:00 +0200

    Models done, except the review

---
+++ models/Artwork.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const artworkSchema = new Schema({
+  city : {type: String, required : true},
+  location : {longitude: Number, latitude: Number},
+  artist : {type: String, required : true},
+  reviews : [{type: objectId, ref: Review}],
+  likes : {type: Number},
+  visits : {type: Number},
+  pictures : {type: String, required : true},
+  incognito : {type: Boolean, required : true}
+},{ 
+  timestamps : true
+});
+
+const User = mongoose.model('User', userSchema, 'users');
+
+module.exports = User;
--- models/User.js
+++ models/User.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username : String,
-  password : String,
+  userName : {type: String, required : true},
+  firstName : {type: String, required : true},
+  lastName : {type: String, required : true},
+  email : {type: String, required : true},
+  password : {type: String, required : true},
+  profilePicture : {type: String, required : true},
+  visits : {type: Number},
+  level : {type: Number},
+  userType : {type: String, enum :['tourist','artist'], required : true},
+  artistArtworks: [{type: ObjectId , ref: Artwork }],
+  visitedArtworks: [{type: ObjectId , ref: Artwork }]
 },{ 
   timestamps : true
 });
