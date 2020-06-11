7e5c8b8f48adcbeb3c2bca2460e2f24d34d4feb3
Nardo Kraaijeveld <nardo.kra@gmail.com>
2020-06-11 14:19:04 +0200

    generic form styling applied and a few other small styling changes

--- routes/user/editProfile.js
+++ routes/user/editProfile.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router  = express.Router();
 const User = require('../../models/User');
 const bcrypt = require('bcrypt');
+const cloudinary = require('cloudinary').v2;
 
-
+router.get('/editprofile', (req, res, next)=>{
+    
+})
 
 module.exports = router;
